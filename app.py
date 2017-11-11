2e7665d1ebee00b297a155c5b72c19d9259b3885
Samantha Causey <causeys01@gmail.com>
2017-11-11 16:05:38 -0500

    modified app.py

--- app.py
+++ app.py
@@ -3,25 +3,18 @@ import sys
 import json
 import requests
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def verify():
-    if requests.args.get('hub.mode') == 'subscribe' and request.args.get('hub.challenge'):
+    if request.args.get('hub.mode') == 'subscribe' and request.args.get('hub.challenge'):
         if not request.args.get('hub.verify_token') == os.environ['VERIFY_TOKEN']:
             return 'Verification token misatch', 403
         return request.args['hub.challenge'], 200
 
-    return
-
-    """
-    <h1>Hello heroku</h1>
-    <p>It works!</p>
-
-    <img src="http://loremflickr.com/600/400">
-    """.format(time=the_time)
+    return 'hello heroku', 200
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
