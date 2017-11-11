1ac945bccc606ee6af2372aded17d46a37085842
Samantha Causey <causeys01@gmail.com>
2017-11-11 15:50:26 -0500

    modified app.py for token verification

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
+import os
+import sys
+import json
+import requests
+
 from flask import Flask
-from datetime import datetime
+
 app = Flask(__name__)
 
-@app.route('/')
-def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+@app.route('/', methods=['GET'])
+def verify():
+    if requests.args.get('hub.mode') == 'subscribe' and request.args.get('hub.challenge'):
+        if not request.args.get('hub.verify_token') == os.environ['VERIFY_TOKEN']:
+            return 'Verification token misatch', 403
+        return request.args['hub.challenge'], 200
 
-    return """
+    return
+
+    """
     <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
+    <p>It works!</p>
 
     <img src="http://loremflickr.com/600/400">
     """.format(time=the_time)
